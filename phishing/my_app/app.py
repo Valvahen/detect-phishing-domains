18911a09ffe3caf959f310664527671cdb11c4cc
Valvahen <118073830+Valvahen@users.noreply.github.com>
2024-07-18 11:00:52 +0530

    Add files via upload

--- phishing/my_app/app.py
+++ phishing/my_app/app.py
@@ -6,17 +6,7 @@ import time
 from .modules.process_child_domain_in_batches import process_child_domains_in_batches
 from .modules.determine_status import determine_status
 import os
-import requests
-import ssl
-from .config import content_cache, title_cache, url_queue, whitelist, processed_filename
-
-# Suppress SSL warnings
-requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
-ssl._create_default_https_context = ssl._create_unverified_context
-
-# Disable SSL certificate verification
-session = requests.Session()
-session.verify = False
+from .config import content_cache, title_cache, url_queue, whitelist
 
 # Flask setup
 app = Flask(__name__)
@@ -24,8 +14,9 @@ CORS(app)
 
 @app.route('/', methods=['POST'])
 def hello_world():
-  print("Hello, World!")
-  
+    print("Hello, World!")
+    return "Hello, World!"
+
 @app.route('/detect-phishing', methods=['POST'])
 def detect_phishing():
     file = request.files['file']
@@ -49,23 +40,18 @@ def detect_phishing():
     # Check which columns exist in the original CSV
     existing_columns = df.columns.tolist()
 
-    # Create a new DataFrame with parent_domain, child_domain, and status
-    new_df = df[['Legitimate Domain', 'Newly Registered Domain']].copy()
-    new_df['status'] = df.apply(determine_status, axis=1, existing_columns=existing_columns)
+    # Apply determine_status and append the status to the original DataFrame
+    df['status'] = df.apply(determine_status, axis=1, existing_columns=existing_columns)
 
-    # Filter out rows with status 'safe'
-    new_df = new_df[new_df['status'] != 'safe']
+    # Save the DataFrame with the status column appended to the original CSV file
+    df.to_csv(csv_filename, index=False)
 
-    # Save the new DataFrame to a new CSV file
-    new_df.to_csv(processed_filename, index=False)
-
-    print("Processed CSV file saved successfully.")
+    print("CSV file with status column saved successfully.")
 
     # Generate link to download CSV
     csv_download_link = f'http://127.0.0.1:5000/download/{csv_filename}'
-    processed_file_download_link = f'http://127.0.0.1:5000/download/{processed_filename}'
 
-    return jsonify({"csv_download_link": csv_download_link, "processed_file_download_link": processed_file_download_link}), 200
+    return jsonify({"csv_download_link": csv_download_link}), 200
   
 @app.route('/download/<filename>', methods=['GET'])
 def download_csv(filename):
